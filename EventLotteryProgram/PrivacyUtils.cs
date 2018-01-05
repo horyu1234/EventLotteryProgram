1f77d920898a42513c1022d8555ecbc8c88749af
horyu1234 <horyu1234@naver.com>
2018-01-05 15:03:12 +0900

    한 줄에 여러 개인 정보가 존재하는 경우 개인정보 가리기가 정상적으로 동작하지 않던 문제 수정

--- EventLotteryProgram/PrivacyUtils.cs
+++ EventLotteryProgram/PrivacyUtils.cs
@@ -11,8 +11,8 @@ namespace EventLotteryProgram
     {
         public static string HideName(string text)
         {
-            const string pattern = @"^[가-힣]{2,4}$";
-            var patternRegex = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            const string pattern = @"[가-힣]{2,4}";
+            var patternRegex = new Regex(pattern, RegexOptions.IgnoreCase);
             var regexResult = patternRegex.Matches(text);
 
             if (regexResult.Count == 0)
@@ -20,21 +20,27 @@ namespace EventLotteryProgram
                 return text;
             }
 
-            var name = regexResult[0].Value;
-            var firstName = name[0];
-            var lastName = name[name.Length - 1];
-
-            string hidedName;
-            if (name.Length == 2)
+            var result = text;
+            foreach (Match regexMatch in regexResult)
             {
-                hidedName = firstName + "*";
-            }
-            else
-            {
-                hidedName = firstName + new string('*', name.Length - 2) + lastName;
+                var name = regexMatch.Value;
+                var firstName = name[0];
+                var lastName = name[name.Length - 1];
+
+                string hidedName;
+                if (name.Length == 2)
+                {
+                    hidedName = firstName + "*";
+                }
+                else
+                {
+                    hidedName = firstName + new string('*', name.Length - 2) + lastName;
+                }
+
+                result = result.Replace(name, hidedName);
             }
 
-            return text.Replace(name, hidedName);
+            return result;
         }
 
         public static string HideEmail(string text)
@@ -49,27 +55,34 @@ namespace EventLotteryProgram
                 return text;
             }
 
-            var email = regexResult[0].Value;
-            var emailFront = email.Split('@')[0];
-            var emailDomain = email.Split('@')[1];
-
-            string hidedEmail;
-            if (emailFront.Length < 6)
+            var result = text;
+            foreach (Match regexMatch in regexResult)
             {
-                hidedEmail = new string('*', emailFront.Length) + "@" + emailDomain;
-            }
-            else
-            {
-                hidedEmail = emailFront.Substring(0, 4) + new string('*', emailFront.Length - 4) + "@" + emailDomain;
+                var email = regexMatch.Value;
+                var emailFront = email.Split('@')[0];
+                var emailDomain = email.Split('@')[1];
+
+                string hidedEmail;
+                if (emailFront.Length < 6)
+                {
+                    hidedEmail = new string('*', emailFront.Length) + "@" + emailDomain;
+                }
+                else
+                {
+                    hidedEmail = emailFront.Substring(0, 4) + new string('*', emailFront.Length - 4) + "@" +
+                                 emailDomain;
+                }
+
+                result = result.Replace(email, hidedEmail);
             }
 
-            return text.Replace(email, hidedEmail);
+            return result;
         }
 
         public static string HidePhone(string text)
         {
-            const string normalPhonePattern = @"^(0(2|3[1-3]|4[1-4]|5[1-5]|6[1-4]))-?(\d{3,4})-?(\d{4})$";
-            const string cellPhonePattern = @"^(01[0|1|6|7|8|9])-?(\d{3,4})-?(\d{4})$";
+            const string normalPhonePattern = @"(0(2|3[1-3]|4[1-4]|5[1-5]|6[1-4]))-?(\d{3,4})-?(\d{4})";
+            const string cellPhonePattern = @"(01[0|1|6|7|8|9])-?(\d{3,4})-?(\d{4})";
 
             var normalPhoneRegex = new Regex(normalPhonePattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
             var cellPhoneRegex = new Regex(cellPhonePattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
@@ -81,24 +94,30 @@ namespace EventLotteryProgram
 
             if (normalPhoneRegexResult.Count != 0)
             {
-                var phone = normalPhoneRegexResult[0].Value;
-                var locationCode = normalPhoneRegexResult[0].Result("$1");
-                var first = normalPhoneRegexResult[0].Result("$3");
-                var last = normalPhoneRegexResult[0].Result("$4");
+                foreach (Match regexMatch in normalPhoneRegexResult)
+                {
+                    var phone = regexMatch.Value;
+                    var locationCode = regexMatch.Result("$1");
+                    var first = regexMatch.Result("$3");
+                    var last = regexMatch.Result("$4");
 
-                var hidedPhone = locationCode + "-" + first + "-" + new string('*', last.Length);
-                result = result.Replace(phone, hidedPhone);
+                    var hidedPhone = locationCode + "-" + first + "-" + new string('*', last.Length);
+                    result = result.Replace(phone, hidedPhone);
+                }
             }
 
             if (cellPhoneRegexResult.Count != 0)
             {
-                var phone = cellPhoneRegexResult[0].Value;
-                var locationCode = cellPhoneRegexResult[0].Result("$1");
-                var first = cellPhoneRegexResult[0].Result("$2");
-                var last = cellPhoneRegexResult[0].Result("$3");
+                foreach (Match regexMatch in cellPhoneRegexResult)
+                {
+                    var phone = regexMatch.Value;
+                    var locationCode = regexMatch.Result("$1");
+                    var first = regexMatch.Result("$2");
+                    var last = regexMatch.Result("$3");
 
-                var hidedPhone = locationCode + "-" + first + "-" + new string('*', last.Length);
-                result = result.Replace(phone, hidedPhone);
+                    var hidedPhone = locationCode + "-" + first + "-" + new string('*', last.Length);
+                    result = result.Replace(phone, hidedPhone);
+                }
             }
 
             return result;
